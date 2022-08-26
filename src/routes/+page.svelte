c84e54c0abddb67b99bd4954479696bc82b6969e
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-08-26 16:13:11 -0400

    ✨ Basic home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Link from '../components/link.svelte';
+
+	const name = 'KaBanks';
+	const tag = '@KaBanks';
+	const image = 'https://avatars.githubusercontent.com/u/20971850?v=4';
+	const description = 'A Svelte app to help you manage your bank accounts.';
+	const verified = true;
+	const links = [
+		{
+			title: 'GitHub',
+			link: 'https://github.com/KaBankz',
+			leftIcon: 'https://avatars.githubusercontent.com/u/20971850?v=4',
+			rightIcon: 'https://avatars.githubusercontent.com/u/20971850?v=4'
+		},
+		{
+			title: 'Twitter',
+			link: 'https://twitter.com/KaBankz',
+			leftIcon: '',
+			rightIcon: ''
+		},
+		{
+			title: 'LinkedIn',
+			link: 'https://www.linkedin.com/in/kabanks/',
+			leftIcon: '',
+			rightIcon: ''
+		}
+	];
+</script>
+
+<div class="flex flex-col gap-2 justify-center items-center align-middle text-center">
+	<img src={image} alt={name} class="rounded-full h-36 shadow-md border-black border-2" />
+	<h2 class="font-bold text-2xl">{tag} {verified ? '✅' : ''}</h2>
+	<p>{description}</p>
+</div>
+
+<div class="flex flex-col">
+	{#each links as link}
+		<Link link={link.link} title={link.title} leftIcon={link.leftIcon} rightIcon={link.rightIcon} />
+	{/each}
+</div>
+
+<div>icon row</div>
